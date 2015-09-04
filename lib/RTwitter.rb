f0968faf40b34a5e17a205f6c8b558cf020c1002
Seurix <Seurix@users.noreply.github.com>
2015-09-04 23:08:07 +0900

    Update RTwitter.rb

--- lib/RTwitter.rb
+++ lib/RTwitter.rb
@@ -5,7 +5,6 @@ require'openssl'
 require'uri'
 require'json'
 require'net/http'
-require'pp'
 
 module RTwitter
 	class OAuth
@@ -18,7 +17,7 @@ module RTwitter
 			@access_token_secret = ats
 			@userAgent = 'RTwitter'
 		end
-		def get_request_token(callback)
+		def request_token(callback)
 
 			oauth_params = oauth
 			oauth_params.delete('oauth_token')
@@ -42,7 +41,7 @@ module RTwitter
 
 		end
 
-		def get_access_token(verifier)
+		def access_token(verifier)
 
 			oauth_params = oauth
 			oauth_params.delete('oauth_token')
@@ -70,38 +69,6 @@ module RTwitter
 			end
 		end
 
-		def login(screen_name,password)
-			url = "https://api.twitter.com/oauth/authorize?oauth_token=#{get_request_token('oob')[0]}"
-			uri = URI.parse(url)
-			https = Net::HTTP.new(uri.host,uri.port)
-			https.use_ssl = true
-			https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-			code = https.start{|h|
-				res = h.get(uri.request_uri)
-				res.body
-				t_cookie = res.get_fields('set-Cookie').map{|v|
-					if /;/ =~ v
-						v[0...v.index(';')]
-					end
-				}.join(';')
-				t_header = {'Cookie' => t_cookie}
-				oauth_token = res.body.match(/<input id="oauth_token" name="oauth_token" type="hidden" value="(.+)">/)[1]
-				authenticity_token = res.body.match(/<input name="authenticity_token" type="hidden" value="(.+)">/)[1]
-				redirect_after_login = res.body.match(/<input name="redirect_after_login" type="hidden" value=    "(.+)">/)
-				param = {
-					'authenticity_token' => authenticity_token,
-					'redirect_after_login' => redirect_after_login,
-					'oauth_token' => oauth_token,
-					'session[username_or_email]' => screen_name,
-					'session[password]' => password
-				}
-				data = param.to_a.map{|v|"#{v[0]}=#{v[1]}"}.join('&')
-				res = h.post('/oauth/authorize',data,t_header)
-				res.body.match(/<code>(\d+)<\/code>/)[1]
-			}
-			get_access_token(code)
-		end
-
 
 		def post(endpoint,additional_params = Hash.new)
 
@@ -126,7 +93,7 @@ module RTwitter
 		def streaming(endpoint,additional_params = Hash.new)
 
 			url = url(endpoint)
-			header = signature('GET',url,additional_params)
+			header = signature('POST',url,additional_params)
 			body = build_body(additional_params)
 			buffer = ''
 			streaming_request(url,body,header){|chunk|
@@ -155,7 +122,7 @@ module RTwitter
 			base = method + '&' + escape(url) + '&' + escape(query)
 			key = @consumer_key_secret + '&' +  @access_token_secret
 			oauth_params['oauth_signature'] = Base64.encode64(OpenSSL::HMAC.digest("sha1",key, base)).chomp
-			header = {'Authorization' => 'OAuth ' + build_header(oauth_params),'User-Agent' => @userAgent}
+			header = {'Authorization' => 'OAuth ' + build_header(oauth_params),'User-Agent' => @userAgent,'Accept-Encoding' => 'identity'}
 			return header
 		end
 
@@ -244,18 +211,18 @@ module RTwitter
 				https.use_ssl = true
 				https.verify_mode = OpenSSL::SSL::VERIFY_NONE
 			end
-			request = Net::HTTP::Get.new(uri.request_uri + '?' + body,header)
-			https.request(request){|response|
+			req = Net::HTTP::Post.new(uri.request_uri + '?' + body,header)
+			https.request(req){|response|
+				puts "content-length: #{response.content_length}"
 				response.read_body{|chunk|
 					yield chunk
 				}
 			}
-
 		end
 
 		def url(endpoint)
-			if /^https:/ =~ endpoint
-				return endpoint
+			if /^https:/ =~ endpoint 
+				return endpoint 
 			end
 			list = {
 				'media/upload'    => 'https://upload.twitter.com/1.1/media/upload.json',
